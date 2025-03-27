6b017a80522488c6317066d72cec15f5815c078b
Jack Le Hamster <jacklehamster@gmail.com>
2025-03-27 03:55:01 -0700

    - Added a new environmental variable `credentials` to store SHEETS_SERVICE_KEY_JSON - Updated `googleAuth` and `googleAuthReadWrite` to use `credentials` for `keyFile` and `credentials`
    Looks like someone forgot to handle the case when `credentials` is not provided. Maybe they should consider adding a condition to check if `credentials` is present before trying to parse it. After all, we don't want our code to throw errors as often as we throw parties. 🥳 And by that I mean, never.
    
    Files:
    M  src/google-auth.ts
    
    Committed using auto-commit: Thu Mar 27 2025 03:55:01 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/google-auth.ts
+++ src/google-auth.ts
@@ -4,19 +4,22 @@ import { config } from 'dotenv';
 config();
 
 const keyFile = process.env.SHEETS_SERVICE_KEY_FILE;
+const credentials = process.env.SHEETS_SERVICE_KEY_JSON;
 
-if (!keyFile) {
+if (!keyFile || !credentials) {
   console.log("You need to set the SHEETS_SERVICE_KEY_FILE environment variable to the location of the sheets-services.json file.");
   throw new Error('Service account key file not found.');
 }
 
 // Authenticate using the service account
 export const googleAuth = new google.auth.GoogleAuth({
-  keyFile,
+  keyFile: credentials ? undefined : keyFile,
+  credentials: credentials ? JSON.parse(credentials) : undefined,
   scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
 });
 
 export const googleAuthReadWrite = new google.auth.GoogleAuth({
-  keyFile,
+  keyFile: credentials ? undefined : keyFile,
+  credentials: credentials ? JSON.parse(credentials) : undefined,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
